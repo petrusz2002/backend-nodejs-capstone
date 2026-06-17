dbd56c403a3e9de54fa2c82dbbc44da6fdc2140c
petrusz2002 <petreusz2002@gmail.com>
2026-06-17 07:17:14 -0400

    backend

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,119 +1,50 @@
 /*jshint esversion: 8 */
-
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
+const path = require('path');
 
 const connectToDatabase = require('./models/db');
+const {loadData} = require("./util/import-mongo/index");
+
 
 const app = express();
-
-app.use("*", cors());
-
+app.use("*",cors());
 const port = 3060;
 
-
-// Connect to MongoDB
-connectToDatabase()
-    .then(() => {
-        pinoLogger.info('Connected to DB');
-    })
-    .catch((e) => {
-        console.error('Failed to connect to DB', e);
-    });
+// Connect to MongoDB; we just do this one time
+connectToDatabase().then(() => {
+    pinoLogger.info('Connected to DB');
+})
+    .catch((e) => console.error('Failed to connect to DB', e));
 
 
 app.use(express.json());
 
-
-// ======================
-// Route imports
-// ======================
-
-
-// Auth API
-// később
-// const authRoutes = require('./routes/authRoutes');
-
-
-// Items API Task 1
-const secondChanceItemsRoutes =
-    require('./routes/secondChanceItemsRoutes');
-
-
-// Search API Task 1
-const searchRoutes =
-    require('./routes/searchRoutes');
-
-
-
+// Route files
+const secondChanceRoutes = require('./routes/secondChanceItemsRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
-app.use(
-    pinoHttp({ logger })
-);
+app.use(pinoHttp({ logger }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-// ======================
 // Use Routes
-// ======================
+app.use('/api/secondchance/items', secondChanceRoutes);
+app.use('/api/secondchance/search', searchRoutes);
 
-
-// Auth API
-// később
-// app.use('/api/auth', authRoutes);
-
-
-// Items API Task 2
-app.use(
-    '/api/secondchance/items',
-    secondChanceItemsRoutes
-);
-
-
-// Search API Task 2
-app.use(
-    '/api/secondchance/search',
-    searchRoutes
-);
-
-
-
-// ======================
-// Error handler
-// ======================
-
+// Global Error Handler
 app.use((err, req, res, next) => {
-
     console.error(err);
-
-    res
-        .status(500)
-        .send('Internal Server Error');
-
+    res.status(500).send('Internal Server Error');
 });
 
-
-
-// Test route
-
-app.get("/", (req, res) => {
-
-    res.send("Inside the server");
-
-});
-
-
-
-// Start server
+app.get("/",(req,res)=>{
+    res.send("Inside the server")
+})
 
 app.listen(port, () => {
-
-    console.log(
-        `Server running on port ${port}`
-    );
-
+    console.log(`Server running on port ${port}`);
 });
